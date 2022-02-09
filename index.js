c9eced850ebab2b9ba10a6e8242db54522fc3fdb
alberto.2.garcia <alberto.2.garcia@atos.net>
2022-02-09 11:22:16 -0600

    Filter table

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const CANT_COUNTRIES_PER_PAGE = 20;
 let pageIndex = 0;
+let allCountriesList;
+let countriesList;
+let start;
 
 function getLanguages(languages) {
   if (languages) {
@@ -59,6 +62,30 @@ function getPageIndex() {
   return 0;
 }
 
+function clearTable() {
+  $('#body-table').html('');
+}
+
+function makeTable(countries, startValue) {
+  if (countries.length > 0) {
+    $('#empty-table-msg').addClass('d-none');
+    $('#countries-table').removeClass('d-none');
+    let table =  $('#body-table');
+    countries.forEach((element, i) => {
+      table.append(tableRow(i + startValue, element))
+    });
+  
+    $('tr').on("click", function(e) {
+      let countryName = e.target.parentElement.children[1].innerHTML;
+      let url = 'https://en.wikipedia.org/api/rest_v1/page/summary/' + countryName;
+      getWikipediaInfo(url);
+    });
+  } else {
+    $('#countries-table').addClass('d-none');
+    $('#empty-table-msg').removeClass('d-none');
+  }
+}
+
 function getCountries() {
   let settings = {
     url: `https://restcountries.com/v3.1/all`,
@@ -67,21 +94,13 @@ function getCountries() {
     contentType: 'application/JSON',
     success: (res) => {
       res.sort((a, b) => { return b.name.official.localeCompare(a.name.official)})
+      allCountriesList = JSON.parse(JSON.stringify(res));
       pageIndex = getPageIndex();
-      let start = pageIndex * CANT_COUNTRIES_PER_PAGE;
-      let countries = res.slice(start, start + CANT_COUNTRIES_PER_PAGE);
+      start = pageIndex * CANT_COUNTRIES_PER_PAGE;
+      countriesList = res.slice(start, start + CANT_COUNTRIES_PER_PAGE);
       createNav(res.length);
 
-      let table =  $('#body-table');
-      countries.forEach((element, i) => {
-        table.append(tableRow(i + start, element))
-      });
-
-      $('tr').on("click", function(e) {
-        let countryName = e.target.parentElement.children[1].innerHTML;
-        let url = 'https://en.wikipedia.org/api/rest_v1/page/summary/' + countryName;
-        getWikipediaInfo(url);
-      });
+      makeTable(countriesList, start);
     },
     error: (err) => {
       console.log('error', err);
@@ -93,25 +112,43 @@ function getCountries() {
 
 function navNumber(number, href) {
   return `<li class="page-item"><a class="page-link" href="${href}">${number}</a></li>`
-
 }
 
 function createNav(size) {
   let number = Math.ceil(size / CANT_COUNTRIES_PER_PAGE);
-  console.log(number);
+  // console.log(number);
   let disabledPrevious = pageIndex == 0;
   let disabledNext = pageIndex == number - 1;
-  $('#pagination').append(`
+  let ul = $('.pagination');
+  ul.append(`
     <li class="page-item ${disabledPrevious ? 'disabled' : ''}">
       <a class="page-link" href="${disabledNext ? '#' : '?page=' + (pageIndex - 1)}">Previous</a>
     </li>`)
   for (let i = 0; i < number; i++) {
-    $('#pagination').append(navNumber(i + 1, '?page=' + i))
+    ul.append(navNumber(i + 1, '?page=' + i))
   }
-  $('#pagination').append(`
+  ul.append(`
     <li class="page-item ${disabledNext ? 'disabled' : ''}">
       <a class="page-link" href="${disabledNext ? '#' : '?page=' + (pageIndex + 1)}">Next</a>
     </li>`)
 }
 
 getCountries();
+
+function filterCountries(countries, input) {
+  return countries.filter(country => country.name.official.toLowerCase().includes(input.toLowerCase()));
+}
+
+function filterEvent() {
+  $('#filter-text').on('input', () => {
+    let input = text = $('#filter-text').val();
+    clearTable();
+    if (input.length >= 3) {
+      makeTable(filterCountries(allCountriesList, input), 0);
+    } else {
+      makeTable(countriesList, start);
+    }
+  })
+}
+
+filterEvent();
