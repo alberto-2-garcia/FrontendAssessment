b38e1b95b57bfea2d0adf88b2ab065ad5cca016e
alberto.2.garcia <alberto.2.garcia@atos.net>
2022-02-11 12:10:15 -0600

    Lesson 3 - getElementsByClassName and getElementsByTagName

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 let banner = document.getElementById('page-banner');
 let bookList = document.getElementById('book-list')
-
 console.log(banner);
 console.log(bookList);
+
+let titles = document.getElementsByClassName('title');
+console.log(titles);
+Array.from(titles).forEach(title => {
+  console.log(title);
+})
+
+let lis = document.getElementsByTagName('li');
+console.log(lis);
+Array.from(lis).forEach(li => {
+  console.log(li);
+})
+
