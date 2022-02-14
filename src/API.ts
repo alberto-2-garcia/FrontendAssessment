a1c18d19f37c88b279af8818e7658aa8b130b370
alberto.2.garcia <alberto.2.garcia@atos.net>
2022-02-14 15:30:41 -0600

    Finished Questions API

--- src/API.ts
+++ src/API.ts
@@ -1,3 +1,16 @@
+import { shuffleArray } from './utils';
+
+export type Question = {
+  category: string;
+  correct_answer: string;
+  difficulty: string;
+  incorrect_answers: string[];
+  question: string;
+  type: string;
+}
+
+export type QuestionState = Question & { answers: string[] }
+
 export enum Difficulty {
   EASY = 'easy',
   MEDIUM = 'medium',
@@ -8,4 +21,10 @@ export const fetchQuizQuestions = async (amount: number, difficulty: Difficulty)
   const ENDPOINT = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`;
   const DATA = await (await fetch(ENDPOINT)).json();
   console.log(DATA);
+  return DATA.results.map((question: Question) => (
+    {
+      ...question,
+      answers: shuffleArray([question.correct_answer, ...question.incorrect_answers])
+    }
+  ));
 }
